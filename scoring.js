5570bbdfca69d27a4f30a37f5a87f9dc24df3ae2
unknown <ujwala.pawnarkar@gmail.com>
2019-09-28 11:31:58 -0400

    created throw function

--- scoring.js
+++ scoring.js
@@ -48,6 +48,10 @@ module.exports.calculateScore = function (player) {
     case 'TE':
       return calculateReceivingScore(player)
     default:
-      return 0
+
+      throw ('Unknown player position (K)')
+
   }
 }
+
+
